c08c1b980dbda6febedd501865ccb56f4e970367
Song Hee-joo <Song Hee-joo@LAPTOP-KPFIVMJI.com>
2025-04-25 06:40:13 +0100

    Limpeza geral

--- .Js/contacto.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("btn-enviar").addEventListener("click", function () {
-      alert("Mensagem enviada com sucesso!");
-    });
-  });
